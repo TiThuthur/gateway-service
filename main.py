3a0749a8d7ce17c6f8eb61ad90600d686fba1239
TiThuthur <thurthur.petit.py@gmail.com>
2025-09-20 15:53:49 +0200

    essai d'ajouter les models en mode partagé

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+import httpx
+from datetime import datetime
+from shared-models import Article, SynthesisRequest, AnalysisResult
+
+#Endpoints des services
+ACADEMIC_SERVICE_URL = "http://academic-data-service:8000"
+AI_SYNTHESIS_SERVICE_URL = "http://ai-synthesis-service:8000"
+
+app = FastAPI(title="Gateway Service", description="Service de communication avec les autres services", version="0.1.0")
