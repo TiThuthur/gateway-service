9eee2e56be6cbd3ae6e79cc80653f639d1e64eb8
TiThuthur <thurthur.petit.py@gmail.com>
2025-09-21 18:12:47 +0200

    gestion de la communication entre les services

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM ubuntu:latest
-LABEL authors="arthu"
-
-ENTRYPOINT ["top", "-b"]
+FROM python:3.13-slim
+LABEL authors="arthur"
+WORKDIR /app
+COPY shared_models/ ./shared_models/
+COPY gateway-service/requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY gateway-service/main.py .
+EXPOSE 8000
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,12 +1,63 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List, Optional
+from http.client import HTTPException
+
+from fastapi import FastAPI
 import httpx
-from datetime import datetime
-from shared-models import Article, SynthesisRequest, AnalysisResult
+from shared_models import Article, SynthesisRequest, AnalysisResult, ReportRequest, AcademicResearch
 
 #Endpoints des services
-ACADEMIC_SERVICE_URL = "http://academic-data-service:8000"
-AI_SYNTHESIS_SERVICE_URL = "http://ai-synthesis-service:8000"
+ACADEMIC_SERVICE_URL = "http://academic-service:8001"
+AI_SYNTHESIS_SERVICE_URL = "http://ai-synthesis-service:8002"
 
 app = FastAPI(title="Gateway Service", description="Service de communication avec les autres services", version="0.1.0")
+
+@app.get("/health")
+async def health():
+    return {"message": "healthy", "service": "gateway-service"}
+
+@app.post("/api/v1/generate-report", response_model=AnalysisResult)
+async def generate_report(request: ReportRequest):
+    try:
+        #1. Construction de la requête pour Academic Service
+        academic_request = AcademicResearch(
+            keywords=request.keywords,
+            authors=request.authors,
+            year_from=request.year_from,
+            year_to=request.year_to,
+            max_results=request.max_results
+        )
+        async with httpx.AsyncClient() as client:
+            academic_response = await client.post(f"{ACADEMIC_SERVICE_URL}/search",
+                                                  json=academic_request.model_dump(),
+                                                  timeout=10)
+            academic_response.raise_for_status()
+            academic_data = academic_response.json()
+        #Vérification qu'on a bien des articles
+        if not academic_data.get("articles"):
+            raise HTTPException("Aucun articles trouvé")
+
+        #3. Construction de la requête pour AI service
+        synthesis_query = SynthesisRequest(
+            articles=academic_data["articles"],
+            synthesis_type=request.synthesis_type,
+            language=request.language,
+            focus_areas=request.focus_areas
+        )
+
+        #4. Appel au service AI
+        async with httpx.AsyncClient() as client:
+            ai_response = await client.post(f"{AI_SYNTHESIS_SERVICE_URL}/synthesize",
+                                            json=synthesis_query.model_dump(),
+                                            timeout=60.0
+                                            )
+            ai_response.raise_for_status()
+            return ai_response.json()
+    except httpx.HTTPError as e:
+        print(e)
+        raise HTTPException(e)
+    except Exception as e:
+        print(e)
+        raise HTTPException(e)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
